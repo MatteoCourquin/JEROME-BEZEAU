0bb2a82be25065d0509e122e20a56b9a4e3c09f4
Matteo <matteo.courquin@fortuneo.com>
2024-10-04 18:36:52 +0200

    :sparkles: Setup Project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import SmoothScrolling from '@/layout/lenis';
+import '@/styles/main.sass';
+import type { AppProps } from 'next/app';
+import { usePathname } from 'next/navigation';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const pathname = usePathname();
+
+  return (
+    <>
+      {pathname?.includes('studio') ? (
+        <Component {...pageProps} />
+      ) : (
+        <SmoothScrolling>
+          <Component {...pageProps} />
+        </SmoothScrolling>
+      )}
+    </>
+  );
 }
