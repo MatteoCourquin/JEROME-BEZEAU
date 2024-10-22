cdcaa98175907eccc70f610886dcf228466299e2
Matteo <matteo.courquin@gmail.com>
2024-10-22 14:35:48 +0200

    :rocket: Improve cursor

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -2,23 +2,31 @@ import { useTouchDevice } from '@/utils/states';
 import { useGSAP } from '@gsap/react';
 import clsx from 'clsx';
 import gsap from 'gsap';
-import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
+
+enum CURSOR_STATE {
+  DEFAULT = 'DEFAULT',
+  SEE_MORE = 'SEE_MORE',
+  LINK = 'LINK',
+}
 
 const Cursor = () => {
   if (useTouchDevice()) return null;
 
+  const router = useRouter();
   const { contextSafe } = useGSAP();
 
-  const cursorRef = useRef<HTMLDivElement>(null);
   const pointerRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLSpanElement>(null);
+
+  const [cursorState, setCursorState] = useState(CURSOR_STATE.DEFAULT);
 
   const moveCursor = contextSafe((e: MouseEvent) => {
-    if (!cursorRef.current) return;
+    if (!pointerRef.current) return;
 
-    cursorRef.current.style.opacity = '1';
+    pointerRef.current.style.opacity = '1';
 
-    gsap.to(cursorRef.current, {
+    gsap.to(pointerRef.current, {
       duration: 0.1,
       x: e.clientX,
       y: e.clientY,
@@ -26,87 +34,8 @@ const Cursor = () => {
   });
 
   const hideCursor = () => {
-    if (!cursorRef.current) return;
-    cursorRef.current.style.opacity = '0';
-  };
-
-  const handleHoverLink = () => {
-    gsap
-      .timeline()
-      .to(pointerRef.current, {
-        rotation: -90,
-        x: -12,
-        duration: 0.3,
-      })
-      .to(pointerRef.current, {
-        width: 60,
-        height: 60,
-        x: -30,
-        y: -30,
-        borderRadius: 999,
-        border: '1px solid #ffffffcc',
-        filter: 'grayscale(0%)',
-        backdropFilter: 'invert(0%)',
-        duration: 0.3,
-        delay: 0.2,
-        ease: 'power3.out',
-      });
-  };
-
-  const handleHoverSeeMore = () => {
-    gsap
-      .timeline()
-      .to(pointerRef.current, {
-        rotation: -90,
-        x: -12,
-        duration: 0.3,
-      })
-      .to(pointerRef.current, {
-        width: 120,
-        height: 120,
-        x: -60,
-        y: -60,
-        borderRadius: 999,
-        filter: 'grayscale(0%)',
-        backdropFilter: 'blur(16px) invert(0%)',
-        background: '#ffffff66',
-        duration: 0.3,
-        delay: 0.2,
-        ease: 'power3.out',
-      })
-      .to(
-        textRef.current,
-        {
-          opacity: 1,
-          duration: 0.3,
-          ease: 'power3.out',
-        },
-        // '-=0.3',
-      );
-  };
-
-  const resetCursorState = () => {
-    gsap
-      .timeline()
-      .to(textRef.current, {
-        opacity: 0,
-        duration: 0.2,
-        ease: 'power2.out',
-      })
-      .to(pointerRef.current, {
-        rotation: 0,
-        x: -6,
-        y: -6,
-        width: 12,
-        height: 12,
-        borderRadius: 0,
-        background: '#ffffff00',
-        border: 'none',
-        filter: 'grayscale(100%)',
-        backdropFilter: 'invert(100%)',
-        duration: 0.3,
-        ease: 'power2.out',
-      });
+    if (!pointerRef.current) return;
+    pointerRef.current.style.opacity = '0';
   };
 
   useEffect(() => {
@@ -114,31 +43,48 @@ const Cursor = () => {
     window.addEventListener('mouseout', hideCursor);
 
     document.querySelectorAll('.see-more').forEach((el) => {
-      el.addEventListener('mouseover', handleHoverSeeMore);
-      el.addEventListener('mouseleave', resetCursorState);
+      el.addEventListener('mouseover', () => setCursorState(CURSOR_STATE.SEE_MORE));
+      el.addEventListener('mouseleave', () => setCursorState(CURSOR_STATE.DEFAULT));
     });
 
     document.querySelectorAll('a').forEach((el) => {
-      el.addEventListener('mouseover', handleHoverLink);
-      el.addEventListener('mouseleave', resetCursorState);
+      el.addEventListener('mouseover', () => setCursorState(CURSOR_STATE.LINK));
+      el.addEventListener('mouseleave', () => setCursorState(CURSOR_STATE.DEFAULT));
     });
-  }, []);
+
+    return () => {
+      window.removeEventListener('mousemove', moveCursor);
+      window.removeEventListener('mouseout', hideCursor);
+
+      document.querySelectorAll('.see-more').forEach((el) => {
+        el.removeEventListener('mouseover', () => setCursorState(CURSOR_STATE.SEE_MORE));
+        el.removeEventListener('mouseleave', () => setCursorState(CURSOR_STATE.DEFAULT));
+      });
+
+      document.querySelectorAll('a').forEach((el) => {
+        el.removeEventListener('mouseover', () => setCursorState(CURSOR_STATE.LINK));
+        el.removeEventListener('mouseleave', () => setCursorState(CURSOR_STATE.DEFAULT));
+      });
+    };
+  }, [router]);
 
   return (
     <>
-      <div
-        ref={cursorRef}
-        className="pointer-events-none fixed left-0 top-0 z-[9999] -translate-x-1/2 -translate-y-1/2 opacity-0"
-      >
+      <div ref={pointerRef} className="pointer-events-none fixed left-0 top-0 z-[9999] opacity-0">
         <div
-          ref={pointerRef}
           className={clsx(
-            'pointer-events-none absolute h-[12px] w-[12px] grayscale backdrop-invert',
+            'pointer-events-none absolute h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 transition-all',
+            cursorState === CURSOR_STATE.DEFAULT && 'scale-[0.1] grayscale backdrop-invert',
+            cursorState === CURSOR_STATE.LINK && 'scale-50 rounded-full border-2 border-white-80',
+            cursorState === CURSOR_STATE.SEE_MORE &&
+              'scale-[1] rounded-full bg-white-40 backdrop-blur-lg',
           )}
         >
           <span
-            ref={textRef}
-            className="inline-block w-full rotate-90 text-center leading-[120px] opacity-0"
+            className={clsx(
+              cursorState === CURSOR_STATE.SEE_MORE ? 'scale-100' : 'scale-0',
+              'pointer-events-none inline-block w-full text-center leading-[120px]',
+            )}
           >
             SEE MORE
           </span>
