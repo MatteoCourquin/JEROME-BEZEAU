a47e6ce5fbb136fda6b6adcee29f9a91c284a480
Matteo <matteo.courquin@gmail.com>
2024-11-18 14:31:07 +0100

    :bug: Fix Scroll Screen loader

--- components/ScreenLoader.tsx
+++ components/ScreenLoader.tsx
@@ -42,19 +42,11 @@ export default function ScreenLoader() {
     const columnsRight = Array.from(columns).slice(columnsNumbers / 2);
 
     const disableScroll = () => {
-      document.body.style.overflow = 'hidden';
       document.body.style.position = 'fixed';
-      document.body.style.width = '100%';
-      document.body.style.top = `-${window.scrollY}px`;
     };
 
     const enableScroll = () => {
-      const scrollY = document.body.style.top;
-      document.body.style.overflow = '';
       document.body.style.position = '';
-      document.body.style.width = '';
-      document.body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
     };
 
     gsap
